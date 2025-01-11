8c3f2545c363a7b60463cc4b76df082946e445ec
charlesmbrady <charlesmbrady@gmail.com>
2025-01-11 12:34:26 -0500

    try registry ref

--- cloudfront_charlesmbrady_services.tf
+++ cloudfront_charlesmbrady_services.tf
@@ -1,6 +1,7 @@
 # Charlesmbrady services
 module "charlesmbrady_services" {
-  source  = "../../modules/cloudfront/"
+  source  = "app.terraform.io/charlava/cloudfront-module/aws"
+  version = "1.1.15"
 
   domain_name         = var.charlesmbrady_services_app_domain_name
   is_external_domain  = false
--- cloudfront_charlesmbrady_website.tf
+++ cloudfront_charlesmbrady_website.tf
@@ -1,6 +1,7 @@
 # Charlesmbrady portfolio website
 module "charlesmbrady_portfolio_site" {
-  source  = "../../modules/cloudfront/"
+  source  = "app.terraform.io/charlava/cloudfront-module/aws"
+  version = "1.1.15"
 
   domain_name         = var.charlesmbrady_website_domain_name
   is_external_domain  = false
