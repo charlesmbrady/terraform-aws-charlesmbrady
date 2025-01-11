75203e1fb9e85fa3fb47bc1c36bb43896e17fa5a
charlesmbrady <charlesmbrady@gmail.com>
2025-01-11 12:06:01 -0500

    update relative file paths for module usage

--- cloudfront_charlesmbrady_website.tf
+++ cloudfront_charlesmbrady_website.tf
@@ -1,6 +1,6 @@
 # Charlesmbrady portfolio website
-module "website" {
-  source  = "../../modules/cloudfront"
+module "charlesmbrady_portfolio_site" {
+  source  = "./modules/cloudfront"
 
   domain_name         = var.charlesmbrady_website_domain_name
   is_external_domain  = false
