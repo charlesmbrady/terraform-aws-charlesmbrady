7275053d344c745ce2f8af76e7e3fc6b056a43d8
charlesmbrady <charlesmbrady@gmail.com>
2025-01-12 16:55:06 -0500

    use lambda with no explicit reserved concurrency

--- lambda_services_middleware.tf
+++ lambda_services_middleware.tf
@@ -8,7 +8,7 @@ locals {
 
 module "services_middleware_lambda" {
   source  = "app.terraform.io/charlava/lambda-module/aws"
-  version = "1.2.6"
+  version = "1.2.7"
 
 
   name     = local.services_middleware_name
