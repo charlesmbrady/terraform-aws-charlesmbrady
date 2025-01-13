d245ca9dfa4041d3ea707095dd0036d5de500231
charlesmbrady <charlesmbrady@gmail.com>
2025-01-12 19:14:36 -0500

    fix trailing comma json policy and invalid resource name ref

--- lambda_services_middleware.tf
+++ lambda_services_middleware.tf
@@ -8,7 +8,7 @@ locals {
 
 module "services_middleware_lambda" {
   source  = "app.terraform.io/charlava/lambda-module/aws"
-  version = "1.2.7"
+  version = "1.2.8"
 
 
   name     = local.services_middleware_name
