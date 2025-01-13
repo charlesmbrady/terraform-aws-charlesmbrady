c63cc88e2669ebc043249dded41e1e370aa6d415
charlesmbrady <charlesmbrady@gmail.com>
2025-01-12 21:08:07 -0500

    update ref to rest api id from module output

--- api_services.tf
+++ api_services.tf
@@ -23,7 +23,7 @@ module "services_api" {
 # Cognito Authorizer
 resource "aws_api_gateway_authorizer" "services_api_cognito_authorizer" {
   name                   = "CognitoUserPoolAuthorizer"
-  rest_api_id            = module.services_api.api_gateway_id
+  rest_api_id            = module.services_api.rest_api_id
   type                   = "COGNITO_USER_POOLS"
   provider_arns          = [aws_cognito_user_pool.charlesmbrady.arn]
 }
