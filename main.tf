ef9a777a7cd81b2c264b55f25f4179c16e136fb7
charlesmbrady <charlesmbrady@gmail.com>
2025-01-15 02:08:57 -0500

    remove redundant/unused permissions boundary

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
 ###############################################################################
 #### Terraform Configuration
 ###############################################################################
-
-resource "aws_iam_policy" "permissions_boundary" {
-  name        = "PermissionsBoundaryPolicy"
-  description = "Permissions boundary policy for managing IAM, Lambda, KMS, API Gateway, CloudFront, EC2, and other services"
-
-  policy = file("../../iam_policies/permissions_boundary.json")
-}
