7741f07a230bddbaeadc55f4f10e3728e0c4bb1d
charlesmbrady <charlesmbrady@gmail.com>
2025-01-21 20:28:17 -0500

    remove unexpected 'projects' still being supplied

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -22,7 +22,6 @@ module "main" {
 
   certificate_arn = "arn:aws:acm:us-east-1:632785536297:certificate/3ca27788-124e-425b-9606-815b81d3326c"
   domain_aliases = ["charlesmbrady.com", "auth.charlesmbrady.com", "api.charlesmbrady.com"]
-  projects = ["portfolio/mockdat", "portfolio/self-driving-car", "portfolio/hotkeys-trainer"]
   hosted_zone_id = data.aws_route53_zone.main_zone.id
   alias_name = "charlesmbrady.com"
 
