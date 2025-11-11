ef6e796322834d114f855cf2e820cac2bc485b21
charlesmbrady <charlesmbrady@gmail.com>
2025-11-11 17:53:07 -0500

    rename 'labs' to 'apps'

--- cloudfront.tf
+++ cloudfront.tf
@@ -22,13 +22,13 @@ module "mockdat" {
   alias_name = var.mockdat_alias_name
 }
 
-module "labs" {
+module "apps" {
   source = "./modules/cloudfront_setup"
 
   environment      = var.environment_tag
   certificate_arn  = var.certificate_arn
-  domain_aliases   = var.labs_domain_aliases
+  domain_aliases   = var.apps_domain_aliases
   hosted_zone_id   = var.hosted_zone_id
-  root_project_name_prefix = var.labs_root_project_name_prefix
-  alias_name = var.labs_alias_name
+  root_project_name_prefix = var.apps_root_project_name_prefix
+  alias_name = var.apps_alias_name
 }
--- cognito_clients.tf
+++ cognito_clients.tf
@@ -29,9 +29,9 @@ resource "aws_cognito_user_pool_client" "apps" {
   allowed_oauth_flows                  = local.allowed_oauth_flows
   allowed_oauth_flows_user_pool_client = local.allowed_oauth_flows_user_pool_client
   allowed_oauth_scopes                 = local.allowed_oauth_scopes
-  callback_urls                        = var.cognito_client_labs_callback_urls
-  default_redirect_uri                 = var.cognito_client_labs_default_redirect_uri
-  logout_urls                          = var.cognito_client_labs_logout_urls
+  callback_urls                        = var.cognito_client_apps_callback_urls
+  default_redirect_uri                 = var.cognito_client_apps_default_redirect_uri
+  logout_urls                          = var.cognito_client_apps_logout_urls
   supported_identity_providers         = local.supported_identity_providers
   access_token_validity                = local.access_token_validity
   id_token_validity                    = local.id_token_validity
