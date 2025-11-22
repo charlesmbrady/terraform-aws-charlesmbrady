a1aae3a9617e3c2c24160645dd228827889e3b97
charlesmbrady <charlesmbrady@gmail.com>
2025-11-22 01:02:32 -0500

    udpate aws provider version

--- versions.tf
+++ versions.tf
@@ -1,8 +1,14 @@
 
 terraform {
   required_providers {
-    aws    = ">= 5.29.0"
-    random = ">= 3.1.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.22.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
   }
   required_version = ">= 1.0.0"
 }
