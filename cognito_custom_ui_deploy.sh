c2a5b74f61afdcde45e0fca960598f767c92d37d
charlesmbrady <charlesmbrady@gmail.com>
2025-12-04 01:08:33 -0500

    Add new logo, update css

--- cognito_custom_ui_deploy.sh
+++ cognito_custom_ui_deploy.sh
@@ -1,5 +1,5 @@
 aws cognito-idp set-ui-customization \
-  --user-pool-id us-east-1_YIYfB6zj2 \
+  --user-pool-id 	us-east-1_CKbYONuNe \
   --client-id ALL \
   --css file://$(pwd)/cognito_custom_ui.css \
-  --image-file fileb://$(pwd)/charles_portrait.png
+  --image-file fileb://$(pwd)/CharlesBrady_logo.png
