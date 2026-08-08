7b0d1045227c1a3ac71add109796aa496a771916
charlesmbrady <charlesmbrady@gmail.com>
2026-08-08 18:27:18 -0400

    turn on comfy ui for experimentation

--- environments/test/outputs.tf
+++ environments/test/outputs.tf
@@ -14,3 +14,15 @@ output "region" {
 output "availability_zone_primary_name" {
   value = module.main.availability_zone_primary_name
 }
+
+output "comfyui_instance_id" {
+  value = module.main.comfyui_instance_id
+}
+
+output "comfyui_instance_public_ip" {
+  value = module.main.comfyui_instance_public_ip
+}
+
+output "comfyui_ssm_shell_command" {
+  value = module.main.comfyui_ssm_shell_command
+}
